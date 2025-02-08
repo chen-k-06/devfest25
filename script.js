0ae7189dad8047897b4ce4bfdbfbf6208e5e6081
aduan-git <alexandra.duan24@gmail.com>
2025-02-08 15:53:44 -0500

    Move function to script.js

--- script.js
+++ script.js
@@ -1,5 +1,39 @@
 let map, directionsService, directionsRenderer;
 
+const buildings = [
+    { name: "Library", lat: 40.7128, lng: -74.0060, accessible: true },
+    { name: "Science Hall", lat: 40.7130, lng: -74.0075, accessible: false }
+  ];
+
+function initMap() {
+    const campusCenter = { lat: 40.809008176956404, lng: -73.96384528956837 };
+    // Initialize map
+    const map = new google.maps.Map(document.getElementById("map"), {
+      zoom: 16,
+      center: campusCenter,
+    });
+
+    // Add markers
+    buildings.forEach(building => {
+      const markerColor = building.accessible ? "green" : "red";
+
+      const marker = new google.maps.Marker({
+        position: { lat: building.lat, lng: building.lng },
+        map: map,
+        title: building.name,
+        icon: `http://maps.google.com/mapfiles/ms/icons/${markerColor}-dot.png`
+      });
+
+      const infowindow = new google.maps.InfoWindow({
+        content: `<strong>${building.name}</strong><br>${building.accessible ? "Accessible" : "Not Accessible"}`
+      });
+
+      marker.addListener("click", () => {
+        infowindow.open(map, marker);
+      });
+    });
+  }
+
 function calculateRoute() {
     const start = document.getElementById("start").value;
     const end = document.getElementById("end").value;
