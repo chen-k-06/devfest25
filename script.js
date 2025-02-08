350978540c8147a7b58cf44db3b68b49e11991bc
Kexin <kaitlynkexinchen@gmail.com>
2025-02-08 20:51:34 +0000

    Updated JS, CSS, HTML

--- script.js
+++ script.js
@@ -1,27 +1,5 @@
 let map, directionsService, directionsRenderer;
 
-function initMap() {
-    // Center the map on 116th St & Broadway, NYC
-    const columbiaLocation = { lat: 40.8075, lng: -73.9626 };
-
-    map = new google.maps.Map(document.getElementById("map"), {
-        center: columbiaLocation,
-        zoom: 14,
-    });
-
-    // Initialize Directions Service & Renderer
-    directionsService = new google.maps.DirectionsService();
-    directionsRenderer = new google.maps.DirectionsRenderer();
-    directionsRenderer.setMap(map);
-
-    // Autocomplete for location inputs
-    const startInput = document.getElementById("start");
-    const endInput = document.getElementById("end");
-
-    new google.maps.places.Autocomplete(startInput);
-    new google.maps.places.Autocomplete(endInput);
-}
-
 function calculateRoute() {
     const start = document.getElementById("start").value;
     const end = document.getElementById("end").value;
