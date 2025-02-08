38f616a6491bcc8240ab59ccaa93f5005db432ec
Kexin <kaitlynkexinchen@gmail.com>
2025-02-08 21:22:05 +0000

    Increased search bar functionality

--- script.js
+++ script.js
@@ -1,54 +1,41 @@
-let map, directionsService, directionsRenderer;
-
-// EDIT BUILDINGS
-const buildings = [
-    { name: "Library", lat: 40.7128, lng: -74.0060, accessible: true },
-    { name: "Science Hall", lat: 40.7130, lng: -74.0075, accessible: false }
-  ];
+let map, directionsService, directionsRenderer, startAutocomplete, endAutocomplete;
 
 function initMap() {
     const campusCenter = { lat: 40.809008176956404, lng: -73.96384528956837 };
+
     // Initialize map
-    const map = new google.maps.Map(document.getElementById("map"), {
-      zoom: 16,
-      center: campusCenter,
+    map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 16,
+        center: campusCenter,
     });
 
-    // Add markers
-    buildings.forEach(building => {
-      const markerColor = building.accessible ? "green" : "red";
-
-      const marker = new google.maps.Marker({
-        position: { lat: building.lat, lng: building.lng },
-        map: map,
-        title: building.name,
-        icon: `http://maps.google.com/mapfiles/ms/icons/${markerColor}-dot.png`
-      });
-
-      const infowindow = new google.maps.InfoWindow({
-        content: `<strong>${building.name}</strong><br>${building.accessible ? "Accessible" : "Not Accessible"}`
-      });
-
-      marker.addListener("click", () => {
-        infowindow.open(map, marker);
-      });
-    });
-  }
-
-function calculateRoute() {
-    const start = document.getElementById("start").value;
-    const end = document.getElementById("end").value;
-
-    const directionsService = new google.maps.DirectionsService();
-    const directionsRenderer = new google.maps.DirectionsRenderer();
-
+    // Initialize Directions Service and Renderer
+    directionsService = new google.maps.DirectionsService();
+    directionsRenderer = new google.maps.DirectionsRenderer();
     directionsRenderer.setMap(map);
 
-    if (!start || !end) {
-        alert("Please enter both start and destination locations.");
+    // Autocomplete for start and end locations
+    startAutocomplete = new google.maps.places.Autocomplete(document.getElementById("searchBarStart"));
+    endAutocomplete = new google.maps.places.Autocomplete(document.getElementById("searchBarEnd"));
+
+    // Listen for place changes to trigger route calculation
+    startAutocomplete.addListener("place_changed", calculateRoute);
+    endAutocomplete.addListener("place_changed", calculateRoute);
+}
+
+function calculateRoute() {
+    const startPlace = startAutocomplete.getPlace();
+    const endPlace = endAutocomplete.getPlace();
+
+    if (!startPlace || !endPlace) {
+        alert("Please select both start and destination locations.");
         return;
     }
 
+    const start = startPlace.geometry.location;
+    const end = endPlace.geometry.location;
+
+    // Directions request for walking
     const request = {
         origin: start,
         destination: end,
@@ -58,8 +45,6 @@ function calculateRoute() {
     directionsService.route(request, (result, status) => {
         if (status === google.maps.DirectionsStatus.OK) {
             directionsRenderer.setDirections(result);
-
-            // Display walking time
             const walkingTime = result.routes[0].legs[0].duration.text;
             alert(`Estimated Walking Time: ${walkingTime}`);
         } else {
